d911c5ccaeec855b1a6c214679b8bda204d44d07
Boushey <boushey@otp546457ots.ucsf.edu>
2017-12-21 13:57:50 -0800

    modularize calculation method

--- exercise_variation.py
+++ exercise_variation.py
@@ -1,9 +1,12 @@
 daily_exercise_log = [30, 30, 45, 0, 0, 30, 25]
 
-delta = []
+def calculate_daily_difference(daily_exercise_log):
+    delta = []
 
-for i in range(1, len(daily_exercise_log)):
-    diff = daily_exercise_log[i] - daily_exercise_log[i-1]
-    delta.append(diff)
+    for i in range(1, len(daily_exercise_log)):
+        diff = daily_exercise_log[i] - daily_exercise_log[i-1]
+        delta.append(diff)
 
-print(delta)
+    return delta
+
+print(calculate_daily_difference(daily_exercise_log))
