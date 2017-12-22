792efc7055c451a36c4b3c24f6e3571b94079649
Boushey <boushey@otp546457ots.ucsf.edu>
2017-12-22 11:30:44 -0800

    use zero as starting difference value

--- exercise_variation.py
+++ exercise_variation.py
@@ -3,6 +3,8 @@ daily_exercise_log = [30, 30, 45, 0, 0, 30, 25]
 def calculate_daily_difference(daily_exercise_log):
     delta = []
 
+    delta.append(0)
+
     for i in range(1, len(daily_exercise_log)):
         diff = daily_exercise_log[i] - daily_exercise_log[i-1]
         delta.append(diff)
